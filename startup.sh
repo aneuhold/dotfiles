886daf5509b8cf17d6d4cd170a563e76ec4a3401
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-09-30 16:00:06 -0700

    Add updating funtionality for main scripts

--- startup.sh
+++ startup.sh
@@ -20,6 +20,13 @@ setup_mac() {
     mac_setup_node
   fi
 
+  if [[ $(has_command "npm") == "true" ]]; then
+    echo "$GREEN_ICON This machine does have npm installed."
+  else
+    echo "$RED_ICON This machine does not have npm installed..."
+    mac_setup_npm
+  fi
+
   # Check if the main scripts are installed
   if [[ -z $(npm list -g | grep $MAIN_SCRIPTS_PKG_NAME) ]]; then
     echo "$RED_ICON $MAIN_SCRIPTS_PKG_NAME is not installed."
@@ -27,6 +34,28 @@ setup_mac() {
   else 
     echo "$GREEN_ICON $MAIN_SCRIPTS_PKG_NAME already installed."
     echo "Checking for updates to $MAIN_SCRIPTS_PKG_NAME..."
+    if [[ $(needs_main_scripts_update) == "true" ]]; then
+      echo "$RED_ICON $MAIN_SCRIPTS_PKG_NAME is outdated."
+
+      # As of 9/30/2021 evidently updating just a single package deletes 
+      # all packages. Really weird.
+      echo "Updating $MAIN_SCRIPTS_PKG_NAME and all global packages...";
+      echo $(npm update -g)
+
+    else
+      echo "$GREEN_ICON $MAIN_SCRIPTS_PKG_NAME is up to date."
+    fi
+  fi
+}
+
+# Checks to see if the main scripts package needs to be updated
+#
+# Returns a string either "true" or "false"
+needs_main_scripts_update() {
+  if [[ -z $(npm outdated -g $MAIN_SCRIPTS_PKG_NAME) ]]; then
+    echo "true"
+  else
+    echo "false"
   fi
 }
 
@@ -62,16 +91,25 @@ mac_setup_node() {
   echo $(brew install node)
 }
 
+mac_setup_npm() {
+  echo "Setting up npm..."
+  echo $(brew reinstall node)
+}
+
 install_main_scripts() {
   echo "Installing $MAIN_SCRIPTS_PKG_NAME...";
   npm i -g $MAIN_SCRIPTS_PKG_NAME;
 }
 
+if [[ "$1" == "update" && "$2" == "mainscripts" ]]; then
+  echo "Updating $MAIN_SCRIPTS_PKG_NAME...";
+  npm update -g;
+  echo "Running $MAIN_SCRIPTS_PKG_NAME command again with the following arguments: $3"
+  tb $3;
 # If this is running on a mac
-if [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "This is running on a mac... Executing appropriate commands..."
   setup_mac
 else
   echo "$RED_ICON Unknown OSTYPE... Please add a command for this machine in the startup.sh script."
 fi
-
